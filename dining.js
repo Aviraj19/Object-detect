b714544f313c8368c61a482e021b8473e55dd09f
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-05-19 14:27:14 +0530

    Add files via upload

---
+++ dining.js
@@ -0,0 +1,43 @@
+status1=""
+objects=[]
+function preload() {
+img=loadImage("dining.jpg")
+}
+function setup() {
+    canvas=createCanvas(800,450)
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd", modelloaded)
+    document.getElementById("status").innerHTML="status : detecting objects"
+}
+ function modelloaded() {
+     console.log("Model is loaded")
+     status1=true
+     objectdetector.detect(img,getresult)
+ }
+ function getresult(error,results) {
+     if (error) {
+         console.log("error")
+     }
+     else {
+        console.log(results)
+        objects = results
+     }
+ }
+ function draw() {
+    image(img,0,0,800,450)
+    if (status1 !="") {
+        for(i=0;i<objects.length;i++) {
+         fill("blue")
+         percent=floor(objects[i].confidence*100)
+         text(objects[i].label+" "+ percent+"%",objects[i].x,objects[i].y)
+         textSize(30)
+         noFill()
+         stroke("red")
+         rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+         document.getElementById("status").innerHTML="status : objects detected"
+        }
+    }
+}
+function back() {
+    window.location="main.html";
+}
---
+++ main.js
@@ -0,0 +1,18 @@
+function room1() {
+    window.location="bathroom.html";
+}
+function room2() {
+    window.location="Livingroom.html";
+}
+function room3() {
+    window.location="dining.html";
+}
+function room4() {
+    window.location="Livingroom2.html";
+}
+function room5() {
+    window.location="garden.html";
+}
+function room6() {
+    window.location="bedroom.html";
+}
